d380fb01fc9ce5f4a32fb26179584087c6208ed7
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-26 17:23:29 +0100

    doc: add class, properties, and methods documentation

--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -2,23 +2,35 @@ using System;
 
 namespace Collections
 {
+    /// <summary>
+    /// Represent a generic user.
+    /// </summary>
     public class User : IUser
     {
+        /// <summary>
+        /// Build a new <see cref="User"/> class.
+        /// </summary>
+        /// <param name="fullName">The first plus last name of the user, it could be <c>null</c>.</param>
+        /// <param name="username">The username of this user, cannot be <c>null</c>.</param>
+        /// <param name="age">The age of the user, it could be <c>null</c>.</param>
         public User(string fullName, string username, uint? age)
         {
             this.Username = username ?? throw new ArgumentNullException(nameof(username));
             this.FullName = fullName;
             this.Age = age;
         }
-        
+
+        /// <inheritdoc cref="IUser.Age"/>
         public uint? Age { get; }
-        
+
+        /// <inheritdoc cref="IUser.FullName"/>
         public string FullName { get; }
-        
+
+        /// <inheritdoc cref="IUser.Username"/>
         public string Username { get; }
 
+        /// <inheritdoc cref="IUser.IsAgeDefined"/>
         public bool IsAgeDefined => this.Age != null;
         
-        // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
     }
 }
